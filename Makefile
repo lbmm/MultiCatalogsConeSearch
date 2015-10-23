34c49c5fd1f9aa6ac1c819e28d550ea7fedf33f6
Federica Moscato <federica.moscato@gmail.com>
2015-10-23 14:38:14 +0200

    added pccs1f30

---
+++ Makefile
@@ -0,0 +1,35 @@
+.PHONY: clean-pyc clean-build docs
+
+help:
+	@echo "clean-build - remove build artifacts"
+	@echo "clean-pyc - remove Python file artifacts"
+	@echo "lint - check style with flake8"
+	@echo "test - run tests quickly with the default Python"
+	@echo "test-all - run tests on every Python version with tox"
+	@echo "sdist - package"
+
+clean: clean-build clean-pyc
+
+clean-build:
+	rm -fr build/
+	rm -fr dist/
+	rm -fr *.egg-info
+
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+
+lint:
+	flake8 MultiCatalogsConeSearch  
+
+test:
+	py.test
+
+test-all:
+	tox
+
+sdist: clean
+	python setup.py sdist
+	ls -l dist
+
--- setup.py
+++ setup.py
@@ -3,25 +3,29 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
+
 config = {
-    'description': 'ASDC prototype for the multicatalog cone search based on MongoDB',
+    'description': 'ASDC Multicatalogs search',
     'author': 'fmoscato',
     'url': '',
-    'version' : 0.1,
+    'version':'0.2', 
     'download_url': 'Where to download it.',
     'author_email': 'fmoscato@asdc.asi.it',
     'install_requires': ['nose', 'pymongo', 'bottle'],
     'packages': ['multicatalogsSearch'],
     'scripts': [],
-    'name': 'multicatalogsSearch',
+    'name': 'MulticatalogsSearch',
     'long_description': open('README.md').read(),
     'entry_points': {
         'console_scripts': [
             'serve = multicatalogsSearch.catalogs:run_server',
-            ]
-        },
-     'package_data':{
-      'multicatalogsSearch': ['static/*', 'js/*.js', 'views/*.tpl'],
+                           ]
+                    },
+    'package_data': {
+        'multicatalogsSearch': ['static/*', 'js/*.js','js/library/*.js', 'views/*.tpl'],
        },
-     'include_package_data': True
+        'include_package_data': True
 }
+
+setup(**config)
